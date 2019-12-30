8ddfd38d2223f15c5cf7cccfd1ca5758855974c3
Art Eschenlauer <esch0041@umn.edu>
2019-12-30 04:10:41 +0000

    added test and example for centering

--- tools/w4mclassfilter/run_serve.sh
+++ tools/w4mclassfilter/run_serve.sh
@@ -2,5 +2,7 @@
 
 # run test and write new output files, showing progress to command line output
 planemo conda_install .
-planemo serve --host 0.0.0.0 --conda_dependency_resolution .
+
+#planemo serve --host 0.0.0.0 --port 8389 --galaxy_root ~/planemo-dev/galaxy-dev .
+planemo serve --host 0.0.0.0 .
 
